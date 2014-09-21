3bb290291de2253934c1e88ce47d1e9f30b7143d
joahCN <448363903@qq.com>
2014-09-21 22:04:57 +0800

    fix UI layout

--- app/scripts/controllers/loginController.js
+++ app/scripts/controllers/loginController.js
@@ -3,12 +3,15 @@
 define(['appModule','service/loginService'], function(module){
     module.register.controller('loginController', ['$scope', 'loginService', '$location', '$rootScope','$cookieStore',function($scope, loginService, $location, $rootScope, $cookieStore){
         $scope.loginInfo = {};
+        $scope.isLoginError = false;
         $scope.login = function(userInfo){
             loginService.userLogin(userInfo, function(results){
                 if(results.status.success){
                     var userProfile = results.results;
                     $rootScope.loginUser = userProfile;
                     $location.path("/userProfile/"+userProfile.id);
+                } else {
+                    $scope.isLoginError = true;
                 }
             });
         };
